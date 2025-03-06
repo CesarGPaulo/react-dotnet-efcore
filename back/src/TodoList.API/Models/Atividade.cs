d2a880449c8822b4520aba9516f01c740b3c748f
Paulo Goncalves <cesargpaulo@gmail.com>
2025-03-06 08:49:32 -0300

    Setup inicial do back em .net5

---
+++ back/src/TodoList.API/Models/Atividade.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.API.Models
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Prioridade { get; set; }
+        public Atividade(int id)
+        {
+           Id = id; 
+        }
+        public Atividade(){}
+    }
+}
